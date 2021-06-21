b7689eac169d8202d45ccf6617dda118ba9937ce
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-06-21 16:16:30 +0200

    czytanie i zapis za pomoca import csv (jeszcze bez poprawnego wyswietlania)

--- reader.py
+++ reader.py
@@ -1,6 +1,30 @@
+"""
+import os
+import json
 
 from sys import argv, stdin, stdout
 
-with open("deniro.csv") as file:
+data = argv[2:]
+with open(argv[1], "a") as file:
+    json.dump(data, file)
+
+
+    if argv[1] != "deniro.csv":
+        print("No such file or directory in: ", os.path.abspath("venv"))
+        print(os.system("DIR"))
     for line in file:
-        stdout.write(file.readline())
+        stdout.write(file.readline())"""
+
+import csv
+
+from sys import argv
+
+with open(argv[1], "r") as file:
+    reader = csv.reader(file)
+    for line in reader:
+        print(line)
+
+with open(argv[1], "a") as file:
+    writer = csv.writer(file)
+    writer.writerow(argv[3:])
+    
