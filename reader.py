1a15a6a92809f4f233f4f4cf245e3119aa9c136a
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-06-21 16:36:39 +0200

    czytanie i zapis za pomoca import csv (jeszcze bez poprawnego wyswietlania)

--- reader.py
+++ reader.py
@@ -1,30 +1,30 @@
-"""
-import os
-import json
-
-from sys import argv, stdin, stdout
-
-data = argv[2:]
-with open(argv[1], "a") as file:
-    json.dump(data, file)
-
-
-    if argv[1] != "deniro.csv":
-        print("No such file or directory in: ", os.path.abspath("venv"))
-        print(os.system("DIR"))
-    for line in file:
-        stdout.write(file.readline())"""
 
 import csv
+import pickle
+import json
 
 from sys import argv
+
+"""
+def jj():
+    data = argv[2:]
+    with open(argv[1], "a") as file:
+        json.dump(data, file)
+        
+
+def pickle_process():
+    data = argv[2:]
+    with open(argv[1], "a") as file:
+        pickle.dump(data, file)
+
+
+def csv_process():"""
 
 with open(argv[1], "r") as file:
     reader = csv.reader(file)
     for line in reader:
         print(line)
 
-with open(argv[1], "a") as file:
+with open(argv[2], "a") as file:
     writer = csv.writer(file)
     writer.writerow(argv[3:])
-    
