27f125341b3fea7187cb4d95e9a4f960bc7f45e3
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-06-24 13:06:26 +0200

    program wykonuje sie poprawnie dla plikow csv

--- test2.py
+++ test2.py
@@ -1,17 +1,29 @@
 import sys
 import csv
 
+
 class DoCsv:
     def __init__(self):
         self.file_content = []
 
-    def read(self):
+    def run_csv(self):
         with open(sys.argv[1], "r") as file:
             reader = csv.reader(file)
             for line in reader:
                 self.file_content.append(line)
-                print(line)
+
+        for line in self.file_content:
+            print(line)
+
+        for param in sys.argv[3:]:
+            param_list = param.split(",")
+            self.file_content[int(param_list[0]) - 1][int(param_list[1]) - 1] = param_list[2]
+
+        with open(sys.argv[2], "w", newline="") as file:
+            writer = csv.writer(file)
+            for line in self.file_content:
+                writer.writerow(line)
 
 
-printuj = DoCsv()
-printuj.read()
+printer = DoCsv()
+printer.run_csv()
